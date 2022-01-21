eb4e7fe6a1de4992eb4197db4c59ba1345176888
jeanpetitt <82118752+jeanpetitt@users.noreply.github.com>
2022-01-21 18:41:47 +0100

    add postgresql

--- findRepeater/production_settings.py
+++ findRepeater/production_settings.py
@@ -1,4 +1,6 @@
+import dj_database_url
 from .settings import *
+
 
 DEBUG = False
 
@@ -8,3 +10,4 @@ SECRET_KEY = 'sy5d2#$j3zbxq*@dgm60--&_3=ie($$$jc=3tdqs=2739^!yqe'
 
 ALLOWED_HOSTS = ['findrep.herokuapp.com']
 
+DATABASES['default'] = dj_database_url.config()
