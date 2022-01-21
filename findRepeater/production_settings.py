d1953554e3d2752ff79913a77ccbf86b349ca136
jeanpetitt <82118752+jeanpetitt@users.noreply.github.com>
2022-01-21 19:04:33 +0100

    white noise

--- findRepeater/production_settings.py
+++ findRepeater/production_settings.py
@@ -11,3 +11,6 @@ SECRET_KEY = 'sy5d2#$j3zbxq*@dgm60--&_3=ie($$$jc=3tdqs=2739^!yqe'
 ALLOWED_HOSTS = ['findrep.herokuapp.com']
 
 DATABASES['default'] = dj_database_url.config()
+
+MIDDLEWARE += ['whitenoise.middleware.WhiteNoiseMiddleware',]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
